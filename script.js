30d60eeed3076de7c5c742dd67950cd25f4ec9e4
Mustafa Dlbs <mdelibaas1@gmail.com>
2024-08-14 16:12:18 +0300

    signin page update

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ function func() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    if (email === 'deneme123@gmail.com' && password === 'deneme123') {
+    if (email === 'delibas123@gmail.com' && password === 'admin123') {
         alert("Login Success");
         window.location.assign("sayfa2.html");
     } else {
         alert("Wrong E-mail or Password");
     }
+}
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => page.classList.remove('active-page'));
+
+    const selectedPage = document.getElementById(pageId);
+    selectedPage.classList.add('active-page');
+
+    const menuItems = document.querySelectorAll('.menu li');
+    menuItems.forEach(item => item.classList.remove('active'));
+
+    const activeItem = document.querySelector(`.menu li a[onclick="showPage('${pageId}')"]`).parentElement;
+    activeItem.classList.add('active');
+}
+
+function viewDetails() {
+    alert("Booking details will be displayed here.");
 }
